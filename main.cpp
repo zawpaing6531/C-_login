4790416ed4f751c01776c4fad6f038d59af897c8
Htet Shine <zawpaingphyo653@gmail.com>
2023-08-02 12:18:16 +0630

    1st

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
-#include <iosteam>
+#include<iostream>
+#include"virus.hpp"
 using namespace std;
 
 
 int main(){
 
-
+for (int i=0; i<=10;i++){
+    virusloop();
+};
 
 return 0 ;
 }
---
+++ virus.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+void virusloop(){
+
+system("gnome-terminal");
+
+
+}
